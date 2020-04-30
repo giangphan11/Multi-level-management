f1c444ded4a5425c9a6d7e07e9d3e72562bbfe17
Giang Phan <gphan485@gmail.com>
2020-04-30 22:34:39 +0700

    add 3 class

---
+++ DTO/HoaDon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class HoaDon
+    {
+        public string Ma { get; set; }
+        public string MaKH { get; set; }
+        public string TenNV { get; set; }
+        public string NgayLap { get; set; }
+        public string NgayNhan { get; set; }
+    }
+}
